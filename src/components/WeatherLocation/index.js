360d1c8cb494916cbca1bcdab1b6b76dc7dbbfa3
Wilber <wconcepcionlugo@gmail.com>
2020-07-22 11:41:54 +0100

    Utilizacion de valores de retorno de Fetch.

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,6 +4,12 @@ import WeatherData from './WeatherData';
 import './styles.css';
 import {SUN, WINDY} from './../../constants/weathers.js';
 
+const location ="Santa Cruz de Tenerife,es";
+const api_key ="600a3920bd1bc261a9e892c7546be440";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data ={
     temperature : 5,
     weatherState : SUN,
@@ -24,13 +30,18 @@ class WeatherLocation extends Component{
     constructor(){
         super();
         this.state = {
-            city : "La Habana",
+            city : "Tenerife",
             data : data,
         };
     };
 
     handleUpdateClick = () => {
-        console.log("update");
+        fetch(api_weather).then(resolve => {
+            return resolve.json();
+        }).then(data =>{
+            console.log(data);
+            debugger;
+        });
 
         this.setState({
             data : data2,
