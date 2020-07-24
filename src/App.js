77e124f25f3b96612d8bd3ddd6880047b54b64e0
Wilber <wconcepcionlugo@gmail.com>
2020-07-24 18:27:30 +0100

    Añadiendo componentes a la vista App.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
-// import LocationList from './components/LocationList';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import LocationList from './components/LocationList';
 // import logo from './logo.svg';
 import './App.css';
 // import { Grid } from '@material-ui/core';
@@ -35,35 +39,51 @@ class App extends Component{
 
   render(){
     return (
-        <Grid fluid>
-          <Row>
-            <h1>Grid System: xs, sm, md, lg</h1>
-          </Row>
-          <Row>
-            <h5> Las columnas se muestran en la misma fila mientras sumen 12 </h5>
-          </Row>
-          <Row around="xs">
-            <Col xs={2}>
-              <div className="green"></div>
-            </Col>
-            <Col xs={2}>
-              <div className="red"></div>
-            </Col>
-            <Col xs={2}>
-              <div className="blue"></div>
-            </Col>
-            <Col xs={2}>
-              <div className="yellow"></div>
-            </Col>
-          </Row>
-        </Grid>
+        // <Grid fluid>
+        //   <Row>
+        //     <h1>Grid System: xs, sm, md, lg</h1>
+        //   </Row>
+        //   <Row>
+        //     <h5> Las columnas se muestran en la misma fila mientras sumen 12 </h5>
+        //   </Row>
+        //   <Row around="xs">
+        //     <Col xs={2}>
+        //       <div className="green"></div>
+        //     </Col>
+        //     <Col xs={2}>
+        //       <div className="red"></div>
+        //     </Col>
+        //     <Col xs={2}>
+        //       <div className="blue"></div>
+        //     </Col>
+        //     <Col xs={2}>
+        //       <div className="yellow"></div>
+        //     </Col>
+        //   </Row>
+        // </Grid>
 
-        // <div className="App">
-        //   <LocationList 
-        //     cities={["Adeje,es", "La Habana,cu", "Artemisa,cu", "Washington,usa", "Lima,pe", "Brasilia,br"]} 
-        //     onSelectedLocation={this.handleSelectedLocation}>
-        //   </LocationList>
-        // </div>
+        <Grid>
+          <Row>
+            <AppBar position="sticky">
+              <Toolbar>
+                <Typography variant="title" color='inherit'>
+                  Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar> 
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList 
+                cities={["Adeje,es", "La Habana,cu", "Artemisa,cu", "Washington,usa", "Lima,pe", "Brasilia,br"]} 
+                onSelectedLocation={this.handleSelectedLocation}>
+              </LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <div className="details"></div>
+            </Col>
+          </Row>        
+        </Grid>
         
     )
   }
