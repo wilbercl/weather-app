2c5ecc4a6a58fd1e73519332ae4ceaf3662f1bc2
Wilber <wconcepcionlugo@gmail.com>
2020-08-03 18:47:07 +0100

    Creando ForecastItem.

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
+import ForecastItem from './ForecastItem';
+import { SUN } from '../constants/weathers';
+
+const days = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+]
+
+const data = {
+    temperature: 10,
+    weatherState: SUN,
+    humidity: 10,
+    wind: "normal",
+}
 
 class ForecastExtended extends Component{
     
+    renderForecastItemDays(){
+        return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+    }
+
     render(){
         const {city} = this.props;
 
         return (<div>
-                    Pronostico Extendido para {city}
+                    <h2 className="forecast-title">Pronostico Extendido para {city}</h2>
+                    {this.renderForecastItemDays()}
                 </div>)
     }
 }
